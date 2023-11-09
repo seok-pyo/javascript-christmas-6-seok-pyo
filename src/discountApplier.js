aa55022889e9394b599bfa2bf3f049637bca15c7
seok-pyo <hongseokpyou@gmail.com>
2023-11-09 21:15:39 +0900

    feat(applier): 날짜에 별이 있는지 확인하는 기능 구현

--- src/discountApplier.js
+++ src/discountApplier.js
@@ -1,6 +1,11 @@
 const applier = {
-  weekend: (day) => {
+  weekendCheck: (day) => {
     if (day % 7 === 1 || day % 7 === 2) return true;
+    return false;
+  },
+
+  starCheck: (day) => {
+    if (day % 7 === 3 || day === 25) return true;
     return false;
   },
 };
