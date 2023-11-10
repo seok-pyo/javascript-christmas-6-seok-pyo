74bca8ba27706f92a0ebc6c0b341f913ed608117
seok-pyo <hongseokpyou@gmail.com>
2023-11-10 17:47:37 +0900

    feat(applier): 증정 이벤트 체크 기능 구현

--- src/discountApplier.js
+++ src/discountApplier.js
@@ -14,16 +14,18 @@ const discount = {
     return false;
   },
 
-  applier: (day) => {
+  applier: (day, totalAmount) => {
     const appliedResult = {
       weekednDiscount: false,
       specialDiscount: 0,
       dDayDiscount: 0,
+      giftEvent: 0,
     };
 
     if (discount.weekendCheck(day)) appliedResult.weekednDiscount = true;
     if (discount.starCheck(day)) appliedResult.specialDiscount = 1000;
     if (discount.dDayCheck(day)) appliedResult.dDayDiscount = 1000 + ((day - 1) * 100);
+    if (totalAmount >= 120_000) appliedResult.giftEvent = 25_000;
 
     return appliedResult;
   },
