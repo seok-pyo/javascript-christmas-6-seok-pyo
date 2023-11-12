e4926955942511ab90e5ee04c42a6666969accfb
seok-pyo <hongseokpyou@gmail.com>
2023-11-12 15:41:04 +0900

    test(inputview): 날짜 입력 검증 테스트

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -4,6 +4,12 @@ describe('입력값 유효성 검증 테스트', () => {
   test('날짜 입력이 1에서 31이 사이가 아닌 경우 예외가 발생한다.', () => {
     const inputDate = '32';
 
-    expect(() => validate.date(inputDate)).toThrowError('1에서 31사이에 숫자를 입력해주세요.');
+    expect(() => validate.date(inputDate)).toThrowError('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+  });
+
+  test('날짜 입력이 숫자가 아닌 경우 예외가 발생한다.', () => {
+    const inputDate = 'a';
+
+    expect(() => validate.date(inputDate)).toThrowError('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
   });
 });
--- src/validation.js
+++ src/validation.js
@@ -1,6 +1,8 @@
 const validate = {
   date(input) {
-    if (Number(input) < 1 || Number(input) > 31) throw new Error('1에서 31사이에 숫자를 입력해주세요.');
+    const reg = /[^0-9]/;
+    if (Number(input) < 1 || Number(input) > 31) throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    if (reg.test(input)) throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
   },
 };
 
