ecd8bb2974667a2b5125656a6c6199b0eea6749b
seok-pyo <hongseokpyou@gmail.com>
2023-11-13 12:27:23 +0900

    feat(validation): 메뉴가 20개를 초과한 케이스 구현

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -14,14 +14,20 @@ describe('입력값 유효성 검증 테스트', () => {
   });
 
   test('메뉴입력이 중복인 경우 예외가 발생한다', () => {
-    const inputMenu = '제로콜라-1, 제로콜라-1';
+    const inputMenu = '제로콜라-1,제로콜라-1';
 
     expect(() => validate.menu(inputMenu)).toThrowError('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
   });
 
   test('메뉴입력이 음료만 있는 경우 예외가 발생한다', () => {
-    const inputMenu = '제로콜라-1, 레드와인-2';
+    const inputMenu = '제로콜라-1,레드와인-2';
 
     expect(() => validate.menu(inputMenu)).toThrowError('[이벤트 안내] 음료만 주문 시, 주문할 수 없습니다.');
+  });
+
+  test('메뉴가 20개를 초과하면 예외가 발생한다.', () => {
+    const inputMenu = '티본스테이크-7,레드와인-6,초코케이크-8';
+
+    expect(() => validate.menu(inputMenu)).toThrowError('[이벤트 안내] 메뉴는 최대 20개까지만 주문할 수 있습니다.');
   });
 });
--- src/validation.js
+++ src/validation.js
@@ -37,6 +37,7 @@ const validate = {
   check(menuArray) {
     const menuNames = [];
     const categoryArray = [];
+    let menuQuantity = 0;
 
     menuArray.forEach((oneMenu) => {
       const [name, quantity] = oneMenu;
@@ -47,8 +48,10 @@ const validate = {
       if (!quantity) throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
       menuNames.push(name);
       categoryArray.push(category);
+      menuQuantity += Number(quantity);
     });
 
+    if (menuQuantity > 20) throw new Error('[이벤트 안내] 메뉴는 최대 20개까지만 주문할 수 있습니다.');
     if (menuNames.length !== new Set(menuNames).size) throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
     if (categoryArray.every((category) => category === 'beverage')) throw new Error('[이벤트 안내] 음료만 주문 시, 주문할 수 없습니다.');
   },
